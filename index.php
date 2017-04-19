f0bfcc5ca57673f617cea032ef0c820087f77436
Mario Cecchi <macecchi@gmail.com>
2017-04-19 16:17:24 -0300

    Nome do banco como parâmetro no .env

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ phpCAS::setNoCasServerValidation();
 phpCAS::forceAuthentication();
 $usuario = phpCAS::getUser();
 $token = strtoupper(substr(base_convert(sha1(uniqid() . rand()), 16, 36), 0, 6));
-$db = new PDO('pgsql:dbname=caronae;host='.DB_HOST.';', DB_USER, DB_PASSWORD);
+$db = new PDO('pgsql:dbname='.DB_NAME.';host='.DB_HOST.';', DB_USER, DB_PASSWORD);
 $consulta = $db->query("SELECT token FROM users WHERE id_ufrj = '$usuario' ");
 $app_token = ($consulta->rowCount())? $consulta->fetchColumn() : NULL;
 $erro = '';
