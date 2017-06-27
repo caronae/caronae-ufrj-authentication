c99c5bb2960549137166fd6c3c7095929b743ec2
Mario Cecchi <mcecchi@thoughtworks.com>
2017-06-26 23:41:03 -0300

    Remove códigos de front-end da nova página

--- CaronaeUFRJAgent.php
+++ CaronaeUFRJAgent.php
@@ -2,11 +2,18 @@
 
 namespace Caronae;
 
+use phpCAS;
+
+require '.env';
+require __DIR__ . '/vendor/autoload.php';
+
 class CaronaeUFRJAgent
 {
     private $siga;
     private $caronae;
     private $adaptor;
+    private $id_ufrj;
+    private $siga_user;
     private $redirect_url;
 
     public function __construct()
@@ -17,20 +24,31 @@ class CaronaeUFRJAgent
         $this->caronae->setInstitution(CARONAE_INSTITUTION_ID, CARONAE_INSTITUTION_PASSWORD);
 
         $this->adaptor = new CaronaeSigaAdaptor;
+
+        phpCAS::client(CAS_VERSION_2_0, 'cas.ufrj.br', 443, '');
+        phpCAS::setNoCasServerValidation();
     }
 
-    public function createOrUpdateUserWithUfrjId(string $id_ufrj)
+    public function authenticateWithIntranet()
     {
-        $siga_user = $this->siga->getProfileById($id_ufrj);
-        $user = $this->adaptor->convertToCaronaeUser($siga_user);
+        phpCAS::forceAuthentication();
+        $this->id_ufrj = phpCAS::getUser();
+    }
+
+    public function fetchUserProfileFromSIGA()
+    {
+        $this->siga_user = $this->siga->getProfileById($this->id_ufrj);
+    }
+
+    public function sendUserToCaronae()
+    {
+        $user = $this->adaptor->convertToCaronaeUser($this->siga_user);
 
         $response = $this->caronae->signUp($user);
         $this->redirect_url = $response->redirect_url;
-
-        $this->redirectToCaronae();
     }
 
-    private function redirectToCaronae()
+    public function redirectToCaronae()
     {
         header('Location: ' . $this->redirect_url, true, 302);
         die;
