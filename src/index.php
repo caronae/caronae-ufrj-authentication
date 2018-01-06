02990295e9676b65573181dbf6251112566ef554
Mario Cecchi <macecchi@gmail.com>
2018-01-06 17:50:28 -0200

    adiciona logs

--- src/index.php
+++ src/index.php
@@ -14,13 +14,19 @@ $agent = new AuthenticationAgent();
 $error = null;
 
 try {
+    error_log('will call authenticateWithIntranet');
     $agent->authenticateWithIntranet();
+    error_log('will call fetchUserProfileFromSIGA');
     $agent->fetchUserProfileFromSIGA();
+    error_log('will call sendUserToCaronae');
     $agent->sendUserToCaronae();
 } catch (\CAS_Exception $e) {
+    error_log('CAS_Exception');
     $error = 'Não foi possível autenticar com a Intranet UFRJ.';
 } catch (\Exception $e) {
+    error_log('CAS_Exception');
     $error = $e->getMessage();
 } finally {
+    error_log('will redirect to caronae with error: '.$error);
     $agent->redirectToCaronae($error);
 }
