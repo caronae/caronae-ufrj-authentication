8def1a4f66196183a26aa1ccf92679abeeec884e
Mario Cecchi <macecchi@gmail.com>
2019-03-16 14:04:45 -0300

    Atualiza script de deploy

--- scripts/update_images.sh
+++ scripts/update_images.sh
@@ -14,10 +14,10 @@ git fetch origin master
 git reset --hard origin/master
 
 echo "Updating images using the tag $CARONAE_ENV_TAG"
-/usr/local/bin/docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull caronae-ufrj-authentication
-/usr/local/bin/docker-compose -f docker-compose.yml -f docker-compose.prod.yml stop caronae-ufrj-authentication
-/usr/local/bin/docker-compose -f docker-compose.yml -f docker-compose.prod.yml rm -f caronae-ufrj-authentication
-/usr/local/bin/docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+/usr/local/bin/docker-compose pull caronae-ufrj-authentication
+/usr/local/bin/docker-compose stop caronae-ufrj-authentication
+/usr/local/bin/docker-compose rm -f caronae-ufrj-authentication
+/usr/local/bin/docker-compose up -d
 
 echo "Clean-up unused Docker images..."
 docker image prune -af
